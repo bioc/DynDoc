340f1dafc7328e2a8d2fbad3729e6c68eb45d193
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-12-17 16:23:57 +0000

    added vigCode temporarily for vExplorer
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DynDoc@2780 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/vignetteCode.R
@@ -0,0 +1,149 @@
+getVignetteCode <- function(vigPath,evalEnv=new.env()) {
+    require(tools) || stop("Requires package tools")
+    chunkList <- Stangle(vigPath,driver=tangleToR)
+    vigInfo <- getVigInfo(vigPath)
+    if ((!is.null(chunkList))&&(!is.null(vigInfo))) {
+        if (is.null(vigInfo$VignettePackage))
+            vigPkg <- "None"
+        else
+            vigPkg <- vigInfo$VignettePackage
+        if (is.null(vigInfo$VignetteDepends))
+            vigDeps <- character()
+        else
+            vigDeps <- vigInfo$VignetteDepends
+
+        vigCode <- new("vignetteCode",chunkList=chunkList,
+                       path=vigPath,
+                       depends=vigDeps,
+                       package=vigPkg,
+                       evalEnv=evalEnv)
+        return(vigCode)
+    }
+    return(NULL)
+}
+
+editVignetteCode <- function(vigCode, pos, code) {
+    chunks <- chunkList(vigCode)
+    setChunk(chunks, pos) <- code
+
+    newVig <- new("vignetteCode",
+                  chunkList=chunks,
+                  path=path(vigCode),
+                  depends=depends(vigCode),
+                  package=package(vigCode),
+                  evalEnv=copyEnv(evalEnv(vigCode)))
+    return(newVig)
+}
+
+.initVignetteCode <- function(where) {
+    setGeneric("vignetteCode", function(object)
+               standardGeneric("vignetteCode"), where=where)
+    setClass("vignetteCode", representation(chunkList="chunkList",
+                                            path="character",
+                                            package="character",
+                                            depends="character",
+                                            evalEnv="environment"),
+             where=where)
+
+    if (is.null(getGeneric("path")))
+        setGeneric("path", function(object)
+                   standardGeneric("path"), where=where)
+    setMethod("path", "vignetteCode", function(object)
+              object@path, where=where)
+
+    if (is.null(getGeneric("package")))
+        setGeneric("package", function(object)
+                   standardGeneric("package"), where=where)
+    setMethod("package", "vignetteCode", function(object)
+              object@package, where=where)
+
+    if (is.null(getGeneric("depends")))
+        setGeneric("depends", function(object)
+                   standardGeneric("depends"), where=where)
+    setMethod("depends", "vignetteCode", function(object)
+              object@depends, where=where)
+
+    if (is.null(getGeneric("chunks")))
+        setGeneric("chunks", function(object)
+                   standardGeneric("chunks"), where=where)
+    setMethod("chunks", "vignetteCode", function(object)
+              chunks(object@chunkList), where=where)
+
+    if (is.null(getGeneric("chunkList")))
+        setGeneric("chunkList", function(object)
+                   standardGeneric("chunkList"), where=where)
+    setMethod("chunkList", "vignetteCode", function(object)
+              object@chunkList, where=where)
+
+    if (is.null(getGeneric("setChunk<-")))
+        setGeneric("setChunk<-", function(object, pos, value)
+                   standardGeneric("setChunk<-"), where=where)
+    setReplaceMethod("setChunk","vignetteCode",
+                     function(object, pos,value) {
+                         setChunk(object@chunkList, pos) <- value
+                         object
+                     }, where=where)
+
+    if (is.null(getGeneric("evalEnv")))
+        setGeneric("evalEnv", function(object)
+                   standardGeneric("evalEnv"), where=where)
+    setMethod("evalEnv", "vignetteCode", function(object)
+              object@evalEnv, where=where)
+
+    if (is.null(getGeneric("numChunks")))
+        setGeneric("numChunks", function(object)
+                   standardGeneric("numChunks"), where=where)
+    setMethod("numChunks","vignetteCode", function(object)
+              numChunks(object@chunkList), where=where)
+    if (is.null(getGeneric("getChunk")))
+        setGeneric("getChunk", function(object, num)
+                   standardGeneric("getChunk"), where=where)
+    setMethod("getChunk","vignetteCode", function(object, num)
+        getChunk(object@chunkList, num), where=where)
+
+    if (is.null(getGeneric("evalChunk")))
+        setGeneric("evalChunk", function(object, pos)
+                   standardGeneric("evalChunk"), where=where)
+    setMethod("evalChunk","vignetteCode", function(object, pos) {
+        chunk <- chunk(getChunk(object, pos))
+        chunkexps <- parse(text=chunk)
+        outVec <- character()
+        if (length(chunkexps) == 0)
+            return(outVec)
+        tmpCon <- textConnection("output","w")
+        sink(file=tmpCon)
+        for (nce in 1:length(chunkexps)) {
+            ce <- chunkexps[[nce]]
+            dce <- deparse(ce, width.cutoff=0.75*getOption("width"))
+            cat(getOption("prompt"),
+                paste(dce, collapse=paste("\n",
+                           getOption("continue"), sep="")),"\n")
+            out <- try(.Internal(eval.with.vis(ce,
+                                               object@evalEnv,
+                                               NULL)))
+             if(inherits(out,"try-error")) {
+                 sink()
+                 close(tmpCon)
+                 stop(out)
+             }
+            if(out$visible) {
+                print(out$value)
+            }
+            cat("\n")
+        }
+        sink()
+        close(tmpCon)
+        output <- paste(output,collapse="\n")
+        paste(output,"\n",sep="")
+    }, where=where)
+
+    setMethod("summary","vignetteCode", function(object) {
+        summary(object@chunkList)
+    }, where=where)
+
+    setMethod("show","vignetteCode", function(object) {
+        show(object@chunkList)
+    }, where=where)
+
+}
+
