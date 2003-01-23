bd410888c2404400565a10e0db6b52f721c70d88
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-01-23 18:25:33 +0000

    Why was DynDoc requiring reposTools?
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DynDoc@3079 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .First.lib <- function(libname,pkgname,where) {
     where <- match(paste("package:", pkgname, sep=""), search())
      require("Biobase")
-     require("reposTools")
      .initDynDocClasses(where)
      .initVignetteCode(where)
 }
