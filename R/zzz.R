bf691fbb74c2faa18da2982f9174391f4343c440
Robert Gentleman <rgentlem@fhcrc.org>
2003-01-25 16:21:14 +0000

    moved a bit more stuff over from Biobase
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DynDoc@3131 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .First.lib <- function(libname,pkgname,where) {
+    require("Biobase", quietly=TRUE) || 
+              stop("cannot load DynDoc without Biobase")
     where <- match(paste("package:", pkgname, sep=""), search())
-     require("Biobase")
      .initDynDocClasses(where)
      .initVignetteCode(where)
 }
