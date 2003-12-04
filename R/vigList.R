423c3a20ee3d24e1b40009ff146254c44a72b369
Jeff Gentry <jgentry@jimmy.harvard.edu>
2003-12-04 18:06:39 +0000

    we removed checkVignetteFile, but still were calling it
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DynDoc@6910 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vigList.R
+++ R/vigList.R
@@ -32,74 +32,69 @@ getVigInfo <- function(vig,pkg=NULL, vigDescFun=baseVigDesc, pkgVers=TRUE) {
     ## Passed a filename, if that file is a vignette, will create
     ## a named list of lists to hold all the vignette metadata
 
-    if (checkVignetteFile(vig)) {
+    file <- readLines(con=vig)
+    lines <- grep("^%[[:space:]]*\\\\Vignette",file)
 
-        file <- readLines(con=vig)
-        lines <- grep("^%[[:space:]]*\\\\Vignette",file)
+    ## Double check that we at least have the VignetteIndexEntry
+    ## (this is probably being caught by the listFilesWithType
+    ##  check, but just in case)
+    if (length(lines) == 0)
+        stop("File ", vig,
+             " does not appear to be a vignette file, ",
+             "no vignette metadata available.")
 
-        ## Double check that we at least have the VignetteIndexEntry
-        ## (this is probably being caught by the listFilesWithType
-        ##  check, but just in case)
-        if (length(lines) == 0)
-            stop("File ", vig,
-                 " does not appear to be a vignette file, ",
-                 "no vignette metadata available.")
+    splitLines <- strsplit(file[lines],"{")
 
-        splitLines <- strsplit(file[lines],"{")
+    listNames <- vector()
+    listNames <- unlist(lapply(splitLines, getVigInfoNames, listNames))
+    newLst <- lapply(splitLines,transformVigInfoLine)
+    ## Remove any NAs from things like \VignetteXXX{}
+    newLst <- lapply(newLst,function(x){if(is.na(x[1])) NULL else x})
+    names(newLst) <- listNames
 
-        listNames <- vector()
-        listNames <- unlist(lapply(splitLines, getVigInfoNames, listNames))
-        newLst <- lapply(splitLines,transformVigInfoLine)
-        ## Remove any NAs from things like \VignetteXXX{}
-        newLst <- lapply(newLst,function(x){if(is.na(x[1])) NULL else x})
-        names(newLst) <- listNames
-
-        ## Add in the path starting from the package base
-        newLst$VigPath <- vig
-        if (!("VignettePackage" %in% names(newLst))) {
-            ## Add in the package name
-            newLst$VignettePackage <- pkg
-        }
-        if ((pkgVers==TRUE)&&(!is.null(pkg))) {
-            ## Add in package version
-            desc <- read.dcf(paste(pkg,"DESCRIPTION",sep="/"))
-            newLst$VignettePkgVersion <- desc[,"Version"]
-        }
-        newLst <- vigDescFun(newLst)
-
-        ## Grab the title as well
-        ## !! Should just be merged in with lines above, but need to
-        ## figure out generic regexps for the string handling
-        line <- grep("\\title{",file)
-        if (length(line) > 0) {
-            splitLine <- strsplit(file[line],"{")
-            tmpTitle <- transformVigInfoLine(splitLine[[1]])
-            if ((is.null(tmpTitle))||(is.na(tmpTitle)))
-                newLst$VignetteTitle <- "Untitled"
-            else {
-                ## Remove any latex
-                tmpTitle <- gsub("\\\\\\w*[[:space:]]","",tmpTitle)
-                newLst$VignetteTitle <- tmpTitle
-            }
-        }
-        else {
-            newLst$VignetteTitle <- "Untitled"
-        }
-
-        ## Determine if there is a PDF file for this vignette, if so get
-        ## the filename
-        pdfFile <- gsub("\\.(Rnw|Snw|rnw|snw)$",".pdf",vig)
-        if (file.exists(pdfFile)) {
-            newLst$PDFpath <- pdfFile
-        }
-        else {
-            newLst$PDFpath <- character()
-        }
-
-        return(newLst)
+    ## Add in the path starting from the package base
+    newLst$VigPath <- vig
+    if (!("VignettePackage" %in% names(newLst))) {
+        ## Add in the package name
+        newLst$VignettePackage <- pkg
     }
-    else
-        FALSE
+    if ((pkgVers==TRUE)&&(!is.null(pkg))) {
+        ## Add in package version
+        desc <- read.dcf(paste(pkg,"DESCRIPTION",sep="/"))
+        newLst$VignettePkgVersion <- desc[,"Version"]
+    }
+    newLst <- vigDescFun(newLst)
+
+    ## Grab the title as well
+    ## !! Should just be merged in with lines above, but need to
+    ## figure out generic regexps for the string handling
+    line <- grep("\\title{",file)
+    if (length(line) > 0) {
+        splitLine <- strsplit(file[line],"{")
+        tmpTitle <- transformVigInfoLine(splitLine[[1]])
+        if ((is.null(tmpTitle))||(is.na(tmpTitle)))
+            newLst$VignetteTitle <- "Untitled"
+        else {
+            ## Remove any latex
+            tmpTitle <- gsub("\\\\\\w*[[:space:]]","",tmpTitle)
+            newLst$VignetteTitle <- tmpTitle
+        }
+    }
+    else {
+        newLst$VignetteTitle <- "Untitled"
+    }
+
+    ## Determine if there is a PDF file for this vignette, if so get
+    ## the filename
+    pdfFile <- gsub("\\.(Rnw|Snw|rnw|snw)$",".pdf",vig)
+    if (file.exists(pdfFile)) {
+        newLst$PDFpath <- pdfFile
+    }
+    else {
+        newLst$PDFpath <- character()
+    }
+
+    return(newLst)
 }
 
 print.pkgFileList <- function(x,...) {
@@ -201,35 +196,31 @@ hasVigHeaderField <- function(vig,field="VignetteIndexEntry") {
 getVignetteHeader <- function(vig, field) {
     ## Will retrieve the metadata information stored for a particular
     ## keyword (or an empty vector if it does not exist).
-    if (checkVignetteFile(vig)) {
-        file <- readLines(con=vig)
-        origPattern <- "^[[:space:]]*%+[[:space:]]*\\\\"
-        if (!missing(field))
-            pattern <- paste(origPattern,field,sep="")
-        else
-            pattern <- origPattern
+    file <- readLines(con=vig)
+    origPattern <- "^[[:space:]]*%+[[:space:]]*\\\\"
+    if (!missing(field))
+        pattern <- paste(origPattern,field,sep="")
+    else
+        pattern <- origPattern
 
-        lines <- grep(pattern,file)
+    lines <- grep(pattern,file)
 
-        if (length(lines) > 0) {
-            ## Separate out the lines that are header and
-            ## then remove the header symbol
-            header <- file[lines]
-            header <- gsub(origPattern,"",header)
-            ## Remove the trailing '}'
-            header <- gsub("}","",header)
-            ## Now have format of 'tag{field'
-            splitHeader <- strsplit(header,"{")
+    if (length(lines) > 0) {
+        ## Separate out the lines that are header and
+        ## then remove the header symbol
+        header <- file[lines]
+        header <- gsub(origPattern,"",header)
+        ## Remove the trailing '}'
+        header <- gsub("}","",header)
+        ## Now have format of 'tag{field'
+        splitHeader <- strsplit(header,"{")
 
-            headerList <- vector(mode="list",length=length(lines))
-            for (i in 1:length(lines)) {
-                names(headerList)[i] <- splitHeader[[i]][1]
-                headerList[[i]] <- splitHeader[[i]][2]
-            }
-            return(headerList)
+        headerList <- vector(mode="list",length=length(lines))
+        for (i in 1:length(lines)) {
+            names(headerList)[i] <- splitHeader[[i]][1]
+            headerList[[i]] <- splitHeader[[i]][2]
         }
-        else
-            return(list())
+        return(headerList)
     }
     else
         return(list())
