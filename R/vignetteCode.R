7091fe6768134d8acb0506e6388162d5eba18bf5
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-03-12 20:17:36 +0000

    updated to reflect the new world order
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DynDoc@7793 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vignetteCode.R
+++ R/vignetteCode.R
@@ -23,54 +23,16 @@ getVignetteCode <- function(vigPath,evalEnv=new.env()) {
 }
 
 editVignetteCode <- function(vigCode, pos, code) {
-    ## copyEnv, list2Env, env2List are copied from Biobase to remove
-    ## the dependency of DynDoc on Biobase
-    env2list <- function(envir, recurse=FALSE) {
-        if (!is.environment(envir))
-            stop("envir argument is not an environment")
 
-        ans <- list()
-        ## This is basically a lot like multiget() except there isn't
-        ## need for checking that what you're looking for exists as
-        ## you know it does
-        vals <- ls(envir)
-        for (i in seq(along=vals))
-            ans[[i]] <- get(vals[i], envir=envir, inherits=FALSE)
-        names(ans) <- vals
+    ## copyEnv comes from Biobase, it is like this to remove the
+    ## dependency
+    copyEnv <- function(oldEnv, newEnv = new.env(hash = TRUE,
+                                parent = parent.env(oldEnv)),
+                        all.names = FALSE)
+    {
 
-        if ((recurse == TRUE)&&(!is.null(parent.env(envir))))
-            ans <- c(ans, list2env(parent.env(envir), recurse))
-
-        ans
-    }
-
-    list2env <- function(vals, envir, recurse=FALSE) {
-        if (!is.environment(envir))
-            stop("envir argument is not an environment")
-
-        if (!is.list(vals))
-            stop("vals argument is not a list")
-
-        len <- length(vals)
-        if (len == 0)
-            return(envir)
-
-        names <- names(vals)
-        if (length(names) != len)
-            stop("Not a properly named list")
-
-        ## !!! Does this work the way it is intended when recurse=TRUE?
-        for (i in 1:len)
-            assign(names[i], vals[[i]], envir=envir, inherits=recurse)
-
-        envir
-    }
-
-    copyEnv <- function(oldEnv, newEnv=new.env(parent=parent.env(oldEnv)),
-                        recurse=FALSE) {
-        oldVals <- env2list(oldEnv, recurse)
-        newEnv <- list2env(oldVals, newEnv)
-        return(newEnv)
+        oldVals <- as.list(oldEnv, all.names)
+        l2e(oldVals, newEnv)
     }
 
     chunks <- chunkList(vigCode)
